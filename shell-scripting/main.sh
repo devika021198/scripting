a4566cdcc805477204a3c6511587d31f815598db
devika021198 <varaladevika1202@gmail.com>
2023-12-28 15:49:56 +0530

    added basic shell scripts into git for practise

---
+++ shell-scripting/main.sh
@@ -0,0 +1,10 @@
+ # I want to create file and write some content into that file
+
+ # create a new file
+touch firstfile.txt
+
+#adding some content into file
+echo "Good Afternoon ! Adding content into file" > firstfile.txt
+
+#view content present inside a file
+cat firstfile.txt
